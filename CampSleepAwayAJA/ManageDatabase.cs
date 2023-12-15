a8543f785e1c7d7979cb660450179fb2405152d8
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2023-12-15 14:54:02 +0100

    createdCRUD methods

--- CampSleepAwayAJA/ManageDatabase.cs
+++ CampSleepAwayAJA/ManageDatabase.cs
@@ -1,12 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CampSleepAwayAJA
+﻿namespace CampSleepAwayAJA
 {
-    internal class ManageDatabase
-    {
-    }
+	public class ManageDatabase
+	{
+		public static void AddCabin()
+		{
+			using var context = new CSAContext();
+			var cabin = new Cabin
+			{
+				CabinName = "Cabin 1"
+			};
+			context.Cabins.Add(cabin);
+			context.SaveChanges();
+		}
+		public static void RemoveCabin()
+		{
+			using var context = new CSAContext();
+			var cabin = context.Cabins.Where(c => c.CabinName == "Cabin 1").FirstOrDefault();
+			context.Cabins.Remove(cabin);
+			context.SaveChanges();
+		}
+
+		public static void AddCamper()
+		{
+			using var context = new CSAContext();
+
+			var camper = new Camper
+			{
+				FirstName = "John",
+				LastName = "Doe",
+				StartDate = new DateTime(2021, 6, 1),
+				EndDate = new DateTime(2021, 6, 30),
+				CabinID = 1,
+
+			};
+		}
+		public static void RemoveCamper()
+		{
+			using var context = new CSAContext();
+			var camper = context.Campers.Where(c => c.FirstName == "John").FirstOrDefault();
+			context.Campers.Remove(camper);
+			context.SaveChanges();
+		}
+
+		public static void AddCounselor()
+		{
+			using var context = new CSAContext();
+
+			var counselor = new Counselor
+			{
+				FirstName = "Jane",
+				LastName = "Doe",
+				StartDate = new DateTime(2021, 6, 1),
+				EndDate = new DateTime(2021, 6, 30),
+				CabinID = 1,
+				ContactInfoID = 1
+			};
+		}
+		public static void RemoveCounselor()
+		{
+			using var context = new CSAContext();
+			var counselor = context.Counselors.Where(c => c.FirstName == "Jane").FirstOrDefault();
+			context.Counselors.Remove(counselor);
+			context.SaveChanges();
+		}
+
+		public static void AddNextOfKin()
+		{
+			var context = new CSAContext();
+
+			var nextOfKin = new NextOfKin
+			{
+				FirstName = "John",
+				LastName = "Doe",
+				Relation = "Father",
+				ContactInfoID = 1
+			};
+		}
+		public static void RemoveNextOfKin()
+		{
+			using var context = new CSAContext();
+			var nextOfKin = context.NextOfKins.Where(c => c.FirstName == "John").FirstOrDefault();
+			context.NextOfKins.Remove(nextOfKin);
+			context.SaveChanges();
+		}
+
+	}
 }
