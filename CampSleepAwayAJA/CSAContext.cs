3638a24a4aa362c584b6ac4588b85f984340b63d
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2023-12-15 12:51:05 +0100

    updated CSAContext

--- CampSleepAwayAJA/CSAContext.cs
+++ CampSleepAwayAJA/CSAContext.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CampSleepAwayAJA
 {
-    public class CSAContext : DbContext
-    {
+	public class CSAContext : DbContext
+	{
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<EmployeeProfile> EmployeesProfile { get; set; }
+		public DbSet<Skill> Skills { get; set; }
 
-    }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var configuration = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.Build();
+
+			var connectionString = configuration.GetConnectionString("Local");
+
+			optionsBuilder.UseSqlServer(connectionString)
+				.LogTo(Console.WriteLine,
+				new[] { DbLoggerCategory.Database.Name },
+				LogLevel.Information)
+				.EnableSensitiveDataLogging();
+		}
+	}
 }
