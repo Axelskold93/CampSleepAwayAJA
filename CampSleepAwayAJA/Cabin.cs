64dac5ad2212ef9757657ba493ff13f92077634b
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2023-12-18 13:27:57 +0100

    Sigge migration

--- CampSleepAwayAJA/Cabin.cs
+++ CampSleepAwayAJA/Cabin.cs
@@ -2,15 +2,15 @@
 
 namespace CampSleepAwayAJA
 {
-    public class Cabin
-    {
-        public int CabinID { get; set; }
-        public required string CabinName { get; set; }
+	public class Cabin
+	{
+		public int CabinID { get; set; }
+		public required string CabinName { get; set; }
 
-        public Counselor Counselor { get; set; }
-
-        
+		public Counselor Counselor { get; set; }
 
 
-    }
+
+
+	}
 }
